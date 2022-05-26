0efceca8fb63bf43800108c5956b6a62031762b1
unknown <lalab996@hotmail.com.tw>
2022-05-27 00:46:21 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from "vuex"
 
 export default createStore({
     state: {
-        loading: false,
+        loading: true,
         init: false,
         ethereum: null,
         web3: null,
