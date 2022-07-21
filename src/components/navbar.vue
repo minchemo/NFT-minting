16036437791478fa6847c2df29322e2dc1280eda
J莫 <lalab996@hotmail.com.tw>
2022-07-21 23:25:18 +0800

    update

---
+++ src/components/navbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="float fixed h-20 w-5/6 bg-[#fafafa] bottom-12 rounded-lg shadow-xl flex items-center justify-between px-12">
+        <div class="h-1/2">
+            <img class="h-full logo" src="@/assets/logo.gif" alt="">
+        </div>
+        <div class="link uppercase flex gap-2">
+            <a class="px-2 py-1 border-transparent border hover:border-gray-500 rounded-md" href="">mint</a>
+            <a class="px-2 py-1 border-transparent border hover:border-gray-500 rounded-md" href="">culture</a>
+            <a class="px-2 py-1 border-transparent border hover:border-gray-500 rounded-md" href="">art</a>
+            <a class="px-2 py-1 border-transparent border hover:border-gray-500 rounded-md" href="">Twitter</a>
+            <a class="px-2 py-1 border-transparent border hover:border-gray-500 rounded-md" href="">opensea</a>
+        </div>
+        <!-- Babys -->
+        <img class="z-[5] w-40 right-[5%] absolute bottom-full -scale-x-100 drop-shadow-xl hover:z-10 hover:scale-125 origin-bottom transition-all"
+            src="@/assets/preview/1.png">
+        <img class=" z-[4] w-40 right-[12%] absolute bottom-full -scale-x-100 drop-shadow-xl hover:z-10 hover:scale-125 origin-bottom transition-all"
+            src="@/assets/preview/2.png">
+        <img class="z-[3] w-40 right-[18%] absolute bottom-full -scale-x-100 drop-shadow-xl hover:z-10 hover:scale-125 origin-bottom transition-all"
+            src="@/assets/preview/3.png">
+        <img class="z-[2] w-40 right-[24%] absolute bottom-full -scale-x-100 drop-shadow-xl hover:z-10 hover:scale-125 origin-bottom transition-all"
+            src="@/assets/preview/5.png">
+        <img class="z-[1] w-40 right-[30%] absolute bottom-full -scale-x-100 drop-shadow-xl hover:z-10 hover:scale-125 origin-bottom transition-all"
+            src="@/assets/preview/4.png">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.float {
+    animation: float 2s alternate-reverse infinite;
+
+    @keyframes float {
+        from {
+            transform: translateY(10%);
+        } to {
+            transform: translateY(0%);
+
+        }
+    }
+}
+.logo {
+    filter: invert(100%) sepia(6%) saturate(19%) hue-rotate(275deg) brightness(114%) contrast(96%);
+}
+</style>
--- src/views/mint.vue
+++ src/views/mint.vue
@@ -1,45 +1,13 @@
 <template>
   <div
-    class="overflow-x-hidden overflow-y-scroll w-screen min-h-screen py-10 flex flex-col bg-black items-center justify-center font-['koulen'] select-none">
-    <video autoplay muted loop class="bg-video">
-      <source src="https://i.imgur.com/USCHX9Q.mp4" type="video/mp4">
-    </video>
+    class="overflow-x-hidden overflow-y-scroll w-screen min-h-screen py-10 flex flex-col bg-gray-700 items-center justify-center font-['Josefin_Sans','sans-serif'] select-none">
 
-    <Title class="relative px-8 mb-20" />
-    <Sale class="line relative px-8 z-20 hover:-translate-y-4 transition-all" />
-    <div class="bg-white z-30 overflow-hidden rounded-full border-black mt-20 w-32 md:w-60">
-      <img class="max-w-full" src="@/assets/preview.gif" alt="" srcset="">
-    </div>
-
+    <!-- <Sale class="line relative px-8 z-20 hover:-translate-y-4 transition-all" /> -->
+    <Navbar />
   </div>
-
 
 </template>
 
-<style lang="scss" scoped>
-.bg-video {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  opacity: .4;
-}
-
-.line {
-  &::after {
-    content: '';
-    width: 5px;
-    height: 150px;
-    background-color: white;
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    transform: translateX(-50%);
-  }
-}
-</style>
 
 <script setup>
 import { ref, onMounted } from "vue"
@@ -48,9 +16,8 @@ import store from "@/store"
 import useEthereum from "@/utils/useEthereum"
 import Sale from "./mint/sale.vue"
 
-import Title from "@/components/title.vue"
-
 import AOS from "aos"
+import Navbar from "../components/navbar.vue"
 
 
 const { init } = useEthereum()
